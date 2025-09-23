60afffc608f4477c7f3eeb13e1bb45b4b42046f9
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-09-23 14:06:20 -0700

    enemy and player collisions make sense

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,5 +1,13 @@
 extends RigidBody2D
 class_name Enemy
 
+var enemy_speed : float = 1
+
 func _ready() -> void:
 	Global.enemy_list.append(self)
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	
+	if Global.player:
+		var dir_to_player = self.global_position.direction_to(Global.player.global_position)
+		state.apply_force(dir_to_player*enemy_speed)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -14,3 +14,9 @@ func _physics_process(delta: float) -> void:
 		velocity= Vector2(move_toward(velocity.x, 0, player_speed), move_toward(velocity.y, 0, player_speed))
 
 	move_and_slide()
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		
+		if collider is RigidBody2D:
+			collider.apply_central_impulse(-collision.get_normal() * .03)
