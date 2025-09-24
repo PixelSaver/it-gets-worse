3982653c2af712ee94d5651b3f6ebd25e28e1247
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-09-23 21:50:01 -0700

    added borders

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -6,3 +6,7 @@ class_name UI
 func _on_health_component_health_changed(new_health: float, max_health:float) -> void:
 	hud_text.text = ""
 	hud_text.append_text("[color=red][font_size=50]H:" + str(int(new_health)) + "/" + str(int(max_health)))
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("escape"):
+		get_tree().paused = !get_tree().paused
