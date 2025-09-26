c53ebc4b5a7064b43686b050f963c9d2406a9aa5
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-09-25 22:17:46 -0700

    pause menu and upgrade menu set up kinda, and seprite wizard downlaoded to parse aseprite stuff

---
+++ UI/revamped/single_upgrade_ui.gd
@@ -0,0 +1,42 @@
+extends Panel
+class_name SingleUIUpgrade
+
+@export var title: RichTextLabel 
+@export var image: TextureRect 
+@export var description: RichTextLabel 
+@export var stored_upgrade : BaseStrategy :
+	set(val):
+		stored_upgrade = val
+		
+		if is_inside_tree():
+			_update_ui()
+		else:
+			call_deferred("_update_ui")
+
+var upgrade_text : String : 
+	set(value):
+		upgrade_text = value
+		title.text = value
+var upgrade_image : Texture2D :
+	set(value):
+		upgrade_image = value
+		image.texture = value
+var upgrade_description : String :
+	set(value):
+		upgrade_description = value
+		description.append_text(value)
+
+func _update_ui():
+	if not stored_upgrade:
+		return
+
+	if title:
+		title.text = stored_upgrade.upgrade_text
+	if image:
+		image.texture = stored_upgrade.texture
+	if description:
+		description.clear()
+		description.append_text(stored_upgrade.upgrade_description)
+
+func _on_pressed() -> void:
+	Global.player.add_upgrade(stored_upgrade)
