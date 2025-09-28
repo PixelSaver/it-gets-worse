83f3a83cf21871720a3426b0b32a94c6c069e827
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-09-28 16:43:25 -0700

    added gun and splash screen and muzzle flash

--- Scripts/gun.gd
+++ Scripts/gun.gd
@@ -1,19 +1,41 @@
-extends Node2D
+extends Sprite2D
 class_name Gun
 
 @onready var bullet_scene = preload("res://Scenes/bullet.tscn")
+@onready var muzzle_flash: Sprite2D = $MuzzleFlash
 var spread : float = .5
 var recoil_str : float = 300
 var multishot : int = 1
 var bullets_per_second = 5
 var timer = 1000000
 
+var hold_distance : float = 100
+
 func _process(delta: float) -> void:
 	timer += delta
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var aim :Vector2= get_parent().global_position.direction_to(
+				get_global_mouse_position()
+			)
+		position = aim * hold_distance
+		
+		rotation = aim.angle()
+		var sign = sign(Vector2.RIGHT.dot(Vector2.from_angle(rotation)))
+		if sign < 0:
+			flip_v = true
+			muzzle_flash.position = Vector2(6.84,-7.16)
+		else:
+			flip_v = false
+			muzzle_flash.position = Vector2(6.84,-15.565)
+
 
 func fire(dir: Vector2) -> bool:
 	if timer < 1/float(bullets_per_second): return false
 	timer = 0
+	
+	muzzle_flash_anim()
 	
 	# normalize the base direction
 	dir = dir.normalized()
@@ -38,6 +60,18 @@ func fire(dir: Vector2) -> bool:
 	
 	return true
 
+func muzzle_flash_anim():
+	muzzle_flash.modulate = Color.WHITE
+	muzzle_flash.rotation_degrees = randfn(150,2)
+	muzzle_flash.scale = Vector2.ONE * randfn(1,0.1)
+	#muzzle_flash.skew = randfn(0,1)
+	muzzle_flash.show()
+	
+	await create_tween().tween_property(
+		muzzle_flash, "modulate", Color(255,255,255,0), 1/float(bullets_per_second)/2
+	).set_trans(Tween.TRANS_QUINT).finished
+	muzzle_flash.hide()
+
 func _spawn_bullet(angle: float) -> void:
 	var curr_bullet : Bullet = bullet_scene.instantiate()
 	var direction = Vector2.from_angle(angle).normalized()
@@ -46,7 +80,7 @@ func _spawn_bullet(angle: float) -> void:
 	curr_bullet.init(direction)
 	curr_bullet.bullet_speed = 1000
 	curr_bullet.bullet_lifetime = 3
-	curr_bullet.global_position = global_position
+	curr_bullet.global_position = muzzle_flash.global_position
 	Global.bullet_cont.add_child(curr_bullet)
 	
 	# apply upgrades
